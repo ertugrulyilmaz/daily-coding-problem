b7c9e7a1e27e3e3b7943c6ce5a6fbe6f32054758
Ertugrul Yilmaz <ert.yil@gmail.com>
2019-10-07 20:04:57 +0100

    add new questions

---
+++ src/main/java/com/daily.coding.problem/others/Question205.java
@@ -0,0 +1,15 @@
+package com.daily.coding.problem.others;
+
+/**
+ * This problem was asked by IBM.
+ *
+ * Given an integer, find the next permutation of it in absolute order. For example, given 48975, the next permutation would be 49578.
+ */
+public class Question205 {
+
+  public static void main(String[] args) {
+    Question205 q = new Question205();
+  }
+
+
+}
